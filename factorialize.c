0d8ab806304857058714bc19b2898b79f2779352
Manuel Pichardo <mlpichardo1@gmail.com>
2018-04-21 15:25:09 +0000

    completed Factorialize and started strContains

--- factorialize.c
+++ factorialize.c
@@ -1,3 +1,22 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int factorialize(long long);
+
+int main(void)
+{
+    int input = get_int("Enter number: ");
+    printf("%i\n", factorialize(input));
+}
+
+int factorialize(long long num)
+{
+    if (num == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return num * factorialize(num - 1);
+    }
+}
---
+++ strContains.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(int argc, string argv[])
+{
+    string fir = argv[1];
+    string sec = argv[2];
+
+    if (argc == 3)
+    {
+        printf("Usage: enter key\n"); // Alert user if key is not entered properly
+        return 1;
+
+        if ()
+        {
+
+        }
+    }
+
+}
